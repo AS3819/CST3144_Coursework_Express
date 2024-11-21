82d954a547cf9cee85e0fc9e72fe04867ee55a1b
AS3819 <AS3819@live.mdx.ac.uk>
2024-11-21 17:15:18 +0000

    Added a post request for orders.

--- Server.js
+++ Server.js
@@ -2,11 +2,13 @@
 
 //Imported functions.
 import express from "express";
+import bodyParser from "body-parser";
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 //Website data.
 const port = 8080;
 let getItemsDatabase;
+let getOrdersDatabase;
 
 //Opens the Express server and created a path for the MongoDB server.
 const app = express();
@@ -24,12 +26,25 @@ const client = new MongoClient(connectionURI, {
 //Connects to the MongoDB server.
 const database = client.db("CST3144_Coursework");
 const itemsCollection = database.collection("Items");
+const ordersCollection = database.collection("Orders");
 
 //Initialises website and database.
 app.use(express.static("Public"));
+app.use(bodyParser.json());
+
+//Function for testing whether code is in the JSON format.
+function testJSON(data) {
+  let value = typeof data !== "string" ? JSON.stringify(data) : data;
+  try {
+    value = JSON.parse(value);
+  } catch (e) {
+    return false;
+  }
+  return typeof value === "object" && value !== null;
+}
 
 //Finds all documents in a collection.
-async function find(collection, print) {
+async function findAll(collection, print) {
   const query = {};
   const results = await collection.find(query).toArray();
   getItemsDatabase = results;
@@ -38,16 +53,45 @@ async function find(collection, print) {
   }
 }
 
-find(itemsCollection, true);
+async function find(collection, searchTerm, print) {
+  const query = {searchTerm};
+  const results = await collection.find(query).toArray();
+  getItemsDatabase = results;
+  if (print) {
+    console.log(results);
+  }
+}
+
+async function insertOne(collection, document) {
+  if (testJSON(document)) {
+    if (testJSON(document)) {
+      const newDocument = document;
+      const result = await collection.insertOne(newDocument);
+      console.log(result);
+    } else {
+      console.log("Error. The data is not in the JSON format.");
+    }
+  }
+}
+
+findAll(itemsCollection, true);
 
 //Function for returning the database in JSON format with a get request.
 function handleItemsGetRequest(request, response) {
-  find(itemsCollection, false);
+  findAll(itemsCollection, false);
   response.send(getItemsDatabase);
 }
 
+//Function for posting an order to the database.
+function handleOrderPostRequest(request, response) {
+  console.log(request);
+  insertOne(ordersCollection, request.body);
+  response.send({"message": "Data posted."});
+}
+
 //The HTTP requests.
-app.get("/Lessons", handleItemsGetRequest);
+app.get("/lessons", handleItemsGetRequest);
+app.post("/orders", handleOrderPostRequest);
 
 //Listens to the port.
 app.listen(port);
